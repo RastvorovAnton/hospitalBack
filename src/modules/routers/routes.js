ba9cdc538a6542efd4efa976ae7bce21a1c44583
Anton Rastvorov <antonrostvorov.exceedteam@gmail.com>
2022-03-29 18:09:16 +0300

    add tables for user and appointment

--- src/modules/routers/routes.js
+++ src/modules/routers/routes.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
+const {
+  getAllUsers,
+  createNewUser,
+} = require("../controllers/user.controller");
+
+router.get("/allUsers", getAllUsers);
+router.post(
+  "/userCreate",
+  [
+    check("email", "Введите верный email").notEmpty(),
+    check("password", "Пароль должен быть длинее 4 символов!").isLength({
+      min: 4,
+      max: 50,
+    }),
+  ],
+  createNewUser
+);
 module.exports = router;
