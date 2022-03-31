23ec70748e629adbcac8479fc4114ab428ae99a1
Anton Rastvorov <antonrostvorov.exceedteam@gmail.com>
2022-03-31 16:13:02 +0300

    create .env file

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+require('dotenv').config()
 
 const apiRoutes = require("./src/modules/routers/routes");
 
 app.use(cors());
 
-const uri = "mongodb+srv://magnifico99:28121999A@cluster0.iscaj.mongodb.net/magnifico99?retryWrites=true&w=majority";
+const uri = process.env.uri;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 app.use("/", apiRoutes);
+
+console.log(uri);
 
 app.listen(8000, () => {
   console.log("8000 port is listening");
